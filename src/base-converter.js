c87193d0e1b1487197f0bf22ae37e744052f9039
Alexander Cheprasov <acheprasov84@gmail.com>
2018-08-19 12:28:24 +0100

    first commit

---
+++ src/base-converter.js
@@ -0,0 +1,73 @@
+const baseConverter = {
+
+    BASE_62: [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+        'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+        'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    ],
+
+    /**
+     * @param {number} number
+     * @param {string[]} base
+     * @return {*|string}
+     */
+    encodeNumber(number, base) {
+        let value = Math.abs(number);
+        const encoded = [];
+        const count = base.length;
+
+        if (count === 1 && number !== 0) {
+            return NaN;
+        }
+        while (value >= count) {
+            let i = value % count;
+            value = Math.floor((value - i) / count);
+            encoded.push(base[i]);
+        }
+        encoded.push(base[value]);
+        if (number < 0) {
+            encoded.push('-');
+        }
+        return encoded.reduce((str, curr) => String(curr) + str, '');
+    },
+
+    /**
+     * @param {string} string
+     * @param {string[]} base
+     * @return {number}
+     */
+    decodeNumber(string, base) {
+        if (isNaN(string)) {
+            return NaN;
+        }
+        const count = base.length;
+        const hash = base.reduce((hash, num, i) => {hash[num] = i; return hash;}, {});
+
+        let number = 0;
+        let str = string;
+        let isNegative = false;
+
+        if (str[0] === '-') {
+            str = str.substr(1, str.length - 1);
+            isNegative = true;
+        }
+
+        let pow = 0;
+        while (str.length > 0) {
+            const chr = str[str.length - 1];
+            str = str.substr(0, str.length - 1);
+            number += (count ** pow) * hash[chr];
+            pow += 1
+        }
+
+        if (isNegative) {
+            number = -number;
+        }
+
+        return number;
+    },
+
+};
+
+module.exports = baseConverter;
